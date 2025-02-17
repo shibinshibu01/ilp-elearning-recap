729c7adbb38ecac6752b70c66d4436e701fb9bc5
Shibin Shibu <shibinsb01@gmail.com>
2025-02-18 03:54:12 +0530

    landing page - second draft

--- script.js
+++ script.js
@@ -5,25 +5,18 @@ const nextButton = document.getElementById('next');
 const cardContainer = document.querySelector('.card-container');
 let scrollAmount = 0;
 
-const cardWidth = cardContainer.querySelector('.day-card').offsetWidth + 20; // Include margin
+const cardWidth = cardContainer.querySelector('.day-card').offsetWidth + 25;
 
 nextButton.addEventListener('click', () => {
-    const maxScroll = cardContainer.scrollWidth - cardContainer.clientWidth;
-    if (scrollAmount < maxScroll) {
-        scrollAmount += cardWidth * 3; 
-        if (scrollAmount > maxScroll) {
-            scrollAmount = maxScroll; 
-        }
+    if (scrollAmount < (cardContainer.scrollWidth - cardContainer.clientWidth)) {
+        scrollAmount += cardContainer.clientWidth / 3; 
         cardContainer.style.transform = `translateX(-${scrollAmount}px)`;
     }
 });
 
 prevButton.addEventListener('click', () => {
     if (scrollAmount > 0) {
-        scrollAmount -= cardWidth * 3; 
-        if (scrollAmount < 0) {
-            scrollAmount = 0; 
-        }
+        scrollAmount -= cardContainer.clientWidth / 3; 
         cardContainer.style.transform = `translateX(-${scrollAmount}px)`;
     }
 });
